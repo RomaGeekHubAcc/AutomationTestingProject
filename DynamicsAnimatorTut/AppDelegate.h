6423b83402ffd4f8c2bc12c273cd0b5d4a751a34
Roman Rybachenko <rom_rybachenko@ukr.net>
2014-03-31 18:31:06 +0300

    Initial Commit

---
+++ DynamicsAnimatorTut/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DynamicsAnimatorTut
+//
+//  Created by Roman Rybachenko on 3/31/14.
+//  Copyright (c) 2014 Roman Rybachenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DynamicsAnimatorTut/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DynamicsAnimatorTut
+//
+//  Created by Roman Rybachenko on 3/31/14.
+//  Copyright (c) 2014 Roman Rybachenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
